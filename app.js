3dede7b9bbc13cdf1a959dfa0facbed266a57cf4
rehana938 <mantinoonerehana934@gmail.com>
2025-12-05 15:48:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// let btn=document.getElementById('btn');
+// btn.addEventListener('click',()=>{
+//     console.log("buttom clicked");
+// } ,{once:true});
+// let cb=documnet.getElementById('cb');
+// let link=documnet.getElementById('link');
+// link.addEventListener('click',(e)=>{
+//     if(!cb.checked){
+//         e.preventDefauilt();
+//     }
+
+// })
+
+// let parent=document.getElementById('parent')
+// let btn=document.getElementById('btn')
+// parent.addEventListener('click',(e)=>{
+//     //console.log(e.target)
+//     console.log(e.currentTarget)
+// })
